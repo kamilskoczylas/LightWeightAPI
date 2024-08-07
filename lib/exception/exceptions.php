1535224679244a406684289b3e4df074b166eb99
Kamil <kamilskoczylas@o2.pl>
2024-08-07 11:52:51 +0200

    Update exceptions.php

--- lib/exception/exceptions.php
+++ lib/exception/exceptions.php
@@ -1,16 +1,9 @@
 <?php
 
-    
-class NoReputationScoreAssertionError extends AssertionError {
-}
-
 
 class AdministrativeAccessRequiredAssertionError extends AssertionError {
 }
 
 
 class SQLParametersRequiredAssertionError extends AssertionError {
-}
-
-class DuplicateVoteAssertionError extends AssertionError {
 }
